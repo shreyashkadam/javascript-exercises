ed4abe5d7777e2a9bb0b49df13de8203ece1ede9
shreyashkadam <shreyash.kadam10@gmail.com>
2022-09-10 22:15:52 +0530

    Complete tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(num) {
+  let cel = ((num - 32)*5)/9;
+  return Math.round(cel*10)/10;
 };
 
-const ctof = function() {
-
+const ctof = function(num) {
+  let feh = (num*9/5) + 32;
+  return Math.round(feh*10)/10;
 };
 
 // Do not edit below this line
