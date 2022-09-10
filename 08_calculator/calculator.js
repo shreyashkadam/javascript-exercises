2acc2b5279d0199033635264d6f5a29942610499
shreyashkadam <shreyash.kadam10@gmail.com>
2022-09-10 22:29:50 +0530

    Complete calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a + b;	
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  for(let i = 0; i < arr.length; i++){
+    sum += arr[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let prod = 1;
+  for(let i = 0; i < arr.length; i++){
+    prod = prod * arr[i];
+  }
+  return prod;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	let pow = 1;
+  for(let i = 0; i < b; i++){
+    pow = pow * a;
+  }
+
+  return pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let fact = 1;
+  for(let i = num; i > 0; i--){
+    fact = fact * i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
