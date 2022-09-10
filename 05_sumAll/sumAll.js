8b824ad6420f69b4c46ae48d23d7bcccccc5b124
shreyashkadam <shreyash.kadam10@gmail.com>
2022-09-10 19:01:17 +0530

    Complete sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
 
+    //validation
+    if(typeof num1 != 'number' || typeof num2 != 'number' || num1 < 0 || num2 < 0){
+        return 'ERROR';
+    }
+
+    //swap numbers if first number is larger
+    if(num1 > num2){
+        let temp = num1;
+        num1 = num2;
+        num2 = temp;
+    }
+
+    for(let i = num1; i <= num2; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
