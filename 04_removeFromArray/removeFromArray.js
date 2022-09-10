bbd5a48c164c6c146e43df29666587a7f7e2e04c
shreyashkadam <shreyash.kadam10@gmail.com>
2022-09-10 13:16:12 +0530

    Complete removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(...theArgs) {
+    let len = theArgs[0].length;
+
+    for(let i = 1; i < theArgs.length; i++){
+        for(let j=0; j < len; j++){
+            if(theArgs[0][j] === theArgs[i]){
+                theArgs[0].splice(j,1);
+                break;
+            }
+        }
+    }
+
+    return theArgs[0];
 
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
