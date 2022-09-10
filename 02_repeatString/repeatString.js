155ddd5fb9f9bc84865f95ad210c0353744c6f59
shreyashkadam <shreyash.kadam10@gmail.com>
2022-09-10 11:42:00 +0530

    Complete repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let newString = '';
+    
+    if(num < 0){
+        return 'ERROR';
+    } else if(num > 0){
+        newString = string;
+        for(let i=0; i < num - 1; i++){
+            newString += string;
+        }
+    }
+    
+    return newString;
 };
 
 // Do not edit below this line
