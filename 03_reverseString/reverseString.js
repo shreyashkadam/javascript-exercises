c4260853b474735524b15fd2198e67f650b3101e
shreyashkadam <shreyash.kadam10@gmail.com>
2022-09-10 12:03:19 +0530

    Complete reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let splittedString = string.split('');
+    let newSplittedString = [];
+    let len = splittedString.length;
 
+    for(let i = 0; i < len; i++){
+        newSplittedString.push(splittedString.pop())
+    }
+    
+    return hey = newSplittedString.join('');
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
