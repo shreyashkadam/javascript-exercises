1fd6623a9da484ec4f26ed2dbf4180e933ddeecc
shreyashkadam <shreyash.kadam10@gmail.com>
2022-09-10 19:15:02 +0530

    Complete leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,17 @@
-const leapYears = function() {
+const leapYears = function (num) {
+    if((num % 4 ==0)){
+        if(num % 400 == 0){
+            return true;
+        } else if(num % 100 == 0){
+            return false;
+        } 
+        return true;
+    }
 
-};
+    return false;
+}
+
+leapYears(1996);
 
 // Do not edit below this line
 module.exports = leapYears;
