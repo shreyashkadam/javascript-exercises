fdbeb311e1c9557b71562440353acb3076045d5b
shreyashkadam <shreyash.kadam10@gmail.com>
2022-09-11 21:09:33 +0530

    Complete palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
+const palindromes = function(string) {
+    let newString = (string.replace(/[^A-Z0-9]+/ig, "")).toLowerCase();
+
+    let splittedString = newString.split('');
+    let newSplittedString = [];
+    let len = splittedString.length;
+
+    for(let i = 0; i < len; i++){
+        newSplittedString.push(splittedString.pop())
+    }
+    
+    let reversed = newSplittedString.join('');
+
+    if(reversed.localeCompare(newString) == 0){
+        return true;
+    } else {
+        return false;
+    }
 
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
