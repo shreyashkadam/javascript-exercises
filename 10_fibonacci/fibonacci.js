1eb6ed64e1eed0e9da22c616c3e8a02ed6220b0a
shreyashkadam <shreyash.kadam10@gmail.com>
2022-09-13 10:14:03 +0530

    Complete fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    let fibArr = [0,1];
 
+    num = Number(num)
+  
+    if(num == 0 || num == 1){
+        return fibArr[num];
+    } else if(num < 0){
+        return 'OOPS'
+    } else {
+        for (let i = 2; i <= num; i++) {
+            fibArr[i] = fibArr[i - 1] + fibArr[i - 2];
+        }
+    }
+
+    return fibArr[num];
 };
+
+fibonacci('10');
 
 // Do not edit below this line
 module.exports = fibonacci;
