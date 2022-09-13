4f1948f72f688156fc2821c24388f769cc0b67d4
shreyashkadam <shreyash.kadam10@gmail.com>
2022-09-13 21:28:17 +0530

    Complete getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,15 @@
-const getTheTitles = function() {
 
+const getTheTitles = function(books) {
+    let arrFin = [];
+    let len = books.length;
+
+    for(let i = 0; i < len; i++){
+        arrFin.push(Object.values(books[i])[0]);
+    }
+
+    return arrFin;
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
